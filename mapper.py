5b94025ab8c9a8e335270f85d8b15ed85168f845
amandakkimball <amanda.kimball@gmail.com>
2019-05-17 15:59:23 +0000

    'lowercase, removed punctuation, removed stopwords'

--- mapper.py
+++ mapper.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 import sys
-#from sklearn.feature_extraction import stop_words
+import string
+from sklearn.feature_extraction import stop_words
+
+# Define stopwords and punctuation
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
+#stopwords = set(['the','and','if'])
+punc = set(string.punctuation)
 
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    
-    # make text all lowercase
+    # remove leading and trailing whitespace and make lowercase
     line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
     
-    # remove punctuation from words
-    #punc = set(string.punctuation)
-    #words = ''.join(char for char in words not in punc)
-    
-    #remove the stopwords
-   # stopwords = set(stop_words.ENGLISH_STOP_WORDS)
-     
-    # for word in words:
-     #  if word not in stopwords:
-      #     print word
-
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        # remove punctuation
+        for char in word:
+            if char in punc:
+                word=word.replace(char,"")
+        # remove stopwords
+        if word not in stopwords:
+            print '%s\t%s' % (word, "1")
